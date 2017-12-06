d4445a311da1f02d20f41f926da1a55556ba0116
Tai <chunchien.tai@104.com.tw>
2017-12-07 00:36:22 +0800

    整理推送通知的部分

--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -38,20 +38,70 @@ class LineBotService
     }
 
     /**
+     * @deprecated
      * @param string $imagePath
      * @param string $directUri
      * @param string $label
      * @return TemplateMessageBuilder
      */
-    public function getImageCarouselColumnTemplateBuilder(
+    public function buildTemplateMessageBuilderDeprecated(
         string $imagePath,
         string $directUri,
         string $label
     ): TemplateMessageBuilder {
-        $target = new UriTemplateActionBuilder($label, $directUri);
-        $target =  new ImageCarouselColumnTemplateBuilder($imagePath, $target);
-        $target = new ImageCarouselTemplateBuilder([$target]);
+        $aa = new UriTemplateActionBuilder($label, $directUri);
+        $bb =  new ImageCarouselColumnTemplateBuilder($imagePath, $aa);
+        $target = new ImageCarouselTemplateBuilder([$bb, $bb, $bb, $bb, $bb, $bb, $bb, $bb]);
 
         return new TemplateMessageBuilder('test123', $target);
+    }
+
+
+    /**
+     * @param array $data
+     *              [
+     *                  [
+     *                      'imagePath' => string,
+     *                      'directUri' => string,
+     *                      'label' => string,
+     *                  ],
+     *              ]
+     * @param string $notificationText
+     * @return array Array of TemplateMessageBuilder
+     */
+    public function buildTemplateMessageBuilder(array $data, string $notificationText = '新通知來囉!'): array
+    {
+        $imageCarouselColumnTemplateBuilders = array_map(function ($d) {
+            return $this->buildImageCarouselColumnTemplateBuilder(
+                $d['imagePath'],
+                $d['directUri'],
+                $d['label']
+            );
+        }, $data);
+
+        $tempChunk = array_chunk($imageCarouselColumnTemplateBuilders, 5);
+        return array_map(function ($data) use ($notificationText) {
+            return new TemplateMessageBuilder(
+                $notificationText,
+                new ImageCarouselTemplateBuilder($data)
+            );
+        }, $tempChunk);
+    }
+
+    /**
+     * @param string $imagePath
+     * @param string $directUri
+     * @param string $label
+     * @return ImageCarouselColumnTemplateBuilder
+     */
+    protected function buildImageCarouselColumnTemplateBuilder(
+        string $imagePath,
+        string $directUri,
+        string $label
+    ): ImageCarouselColumnTemplateBuilder {
+        return new ImageCarouselColumnTemplateBuilder(
+            $imagePath,
+            new UriTemplateActionBuilder($label, $directUri)
+        );
     }
 }
