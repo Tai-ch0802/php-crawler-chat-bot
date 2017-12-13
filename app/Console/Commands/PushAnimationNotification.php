53555507d8042e744eb6de23dda22adcba052fa6
chunchien.tai <i0927935851@gmail.com>
2017-12-13 19:02:12 +0800

    完成command 推送當日動漫指令

--- app/Console/Commands/PushAnimationNotification.php
+++ app/Console/Commands/PushAnimationNotification.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Services\CrawlerService;
+use App\Services\LineBotService;
 use Illuminate\Console\Command;
 
 class PushAnimationNotification extends Command
@@ -11,23 +13,35 @@ class PushAnimationNotification extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'schedule:push-baha-animation';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = '推送巴哈當季動畫通知';
+
+
+    private $path;
+
+    /** @var CrawlerService  */
+    private $crawlerService;
+
+    /** @var LineBotService  */
+    private $lineBotService;
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
+     * PushAnimationNotification constructor.
+     * @param CrawlerService $crawlerService
+     * @param LineBotService $lineBotService
      */
-    public function __construct()
+    public function __construct(CrawlerService $crawlerService, LineBotService $lineBotService)
     {
         parent::__construct();
+        $this->path = config('services.url.baHa');
+        $this->crawlerService = $crawlerService;
+        $this->lineBotService = $lineBotService;
     }
 
     /**
@@ -37,6 +51,37 @@ class PushAnimationNotification extends Command
      */
     public function handle()
     {
-        //
+        $originalData = $this->crawlerService->getOriginalData($this->path);
+        $list = $this->crawlerService->getNewAnimationFromBaHa($originalData);
+
+        $today = today()->format('m/d');
+        $existedList = [];
+        $list = array_map(function ($d) use ($today, &$existedList){
+            if (
+                false === strpos($d['date'], $today) ||
+                in_array($d['label'], $existedList, true)
+            ) {
+                return null;
+            }
+            $existedList[] = $d['label'];
+
+            if (mb_strlen($d['label'], 'UTF-8') > 12) {
+                $d['label'] = mb_substr($d['label'], 0, 9, 'UTF-8') . '...';
+            }
+            return $d;
+        }, $list);
+
+        $target = array_filter($list, function ($d) {
+            return null !== $d;
+        });
+
+        $message = "{$today} 最新動畫來囉！";
+        $messageBuilders = $this->lineBotService->buildTemplateMessageBuilder($target, $message);
+
+        foreach ($messageBuilders as $target) {
+            $this->lineBotService->pushMessage($target);
+        }
+
+        echo "Good luck!\n";
     }
 }
