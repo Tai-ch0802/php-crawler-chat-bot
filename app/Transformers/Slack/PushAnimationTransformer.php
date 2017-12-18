1c56e283702d9123039379fad7dd92b5b4cf3844
Tai <chunchien.tai@104.com.tw>
2017-12-18 21:08:02 +0800

    完成Transformer

--- app/Transformers/Slack/PushAnimationTransformer.php
+++ app/Transformers/Slack/PushAnimationTransformer.php
@@ -10,17 +10,13 @@ class PushAnimationTransformer implements TransformContract
     public function transform($data)
     {
         return [
-            //TODO Author
-            'author_name' => 'Test',
-            'author_link' =>  'https://www.youtube.com/',
-            'author_icon' =>  ':chicken01:',
-            //TODO Title and link
-            'title' => self::class,
-            'title_link' => 'https://tai-service.slack.com/home',
-            //TODO Info 雖然有點宅但這是堅持
-            'text' => "`Testing`  ```This\nshit ``` \n ",
-            //TODO Image 這也是種堅持
-            'image_url' => 'http://img.94201314.net/comicui/170.jpg',
+            'author_name' => $data['authorName'],
+            'author_link' =>  $data['authorLink'],
+            'author_icon' =>  $data['authorIcon'],
+            'title' => $data['label'],
+            'title_link' => $data['directUri'],
+            'text' => $data['text'],
+            'image_url' => $data['imagePath'],
         ];
     }
 }
