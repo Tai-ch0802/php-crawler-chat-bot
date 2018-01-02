7f7398b3894b432f8b7ec70418034546488315ca
Tai-chunchien <i0927935851@gmail.com>
2018-01-03 02:43:42 +0800

    Update ComicList.php

--- app/SlashCommands/ComicList.php
+++ app/SlashCommands/ComicList.php
@@ -30,7 +30,7 @@ class ComicList implements SlashCommandsInterface
     public function buildReply()
     {
         $payload = $this->command['payload'];
-        $currentPage = $payload['page'] ?? 1;
+        $currentPage = $payload['actions'][0]['value'] ?? 1;
         $perPage = 4;
 
         $collection = $this->comicService->getAll();
