21bb214f420370f67e5c2a3d043ecc01a4a94598
Tai-chunchien <i0927935851@gmail.com>
2018-01-04 23:39:20 +0800

    Update SlackService.php

--- app/Services/SlackService.php
+++ app/Services/SlackService.php
@@ -168,6 +168,9 @@ class SlackService
             'fallback' => 'What?',
             'color' => self::ATTACH_COLOR_GREEN,
             'callback_id'  => 'filter_list ' . $serviceName,
+            'mrkdwn_in' => [
+                'text',
+            ],
             'actions' => [
                 [
                     'name' => 'filter',
