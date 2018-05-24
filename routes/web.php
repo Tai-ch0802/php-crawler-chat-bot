7240b040e97ff31c534f6a6b4c124ca1a54362cb
Tai-chunchien <i0927935851@gmail.com>
2018-05-24 17:38:49 +0800

    Remove the trash

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/bilibili_986f22bdf80494fd755c11bc450a6ec0.html', function () {
-    return File::get(public_path() . '/bilibili_986f22bdf80494fd755c11bc450a6ec0.html');
-});
